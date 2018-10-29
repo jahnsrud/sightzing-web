3c1d0d63a1b0fa1ac4150912aaff9636fc4f304a
Markus Jahnsrud <markus@soriko.no>
2018-10-29 10:51:29 +0100

    Tickets page is back on the compiling-wagon

--- sightzing/src/app/app.module.ts
+++ sightzing/src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { TicketsPage } from '../pages/tickets/tickets';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +13,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    TicketsPage,
     TabsPage
   ],
   imports: [
@@ -26,8 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    TicketsPage,
     HomePage,
     TabsPage
   ],
