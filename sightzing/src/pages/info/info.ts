9036cfc295aa9f76406902afc11ef09f68287d71
Andreas Breime Persson <andreasbp@msn.com>
2018-10-29 20:33:10 +0100

    show information function

--- sightzing/src/pages/info/info.ts
+++ sightzing/src/pages/info/info.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the InfoPage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InfoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InfoPage');
+  }
+
+  showInfo() {
+    
   }
 
 }
