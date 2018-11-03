86fd48d1c76df630840c715df21113d4f47a1cf0
MarcusTJensen <31272825+MarcusTJensen@users.noreply.github.com>
2018-11-03 14:04:00 +0100

    random quote displays on today page

--- sightzing/src/pages/home/home.ts
+++ sightzing/src/pages/home/home.ts
@@ -12,6 +12,7 @@ export class HomePage {
   constructor(public navController:NavController, public modalController: ModalController) {
 
     this.getCurrentWeather();
+    this.displayRandomQuote();
 
     if (this.isFirstLaunch()) {
         // presenter welcome
@@ -49,6 +50,53 @@ export class HomePage {
 
     return true;
 
+  }
+  quoteList:string[] = ["Party in the city where the heat is on",
+
+                        "Somebody call 911, shawty's fire burning on the dancefloor",
+                        
+                        "Fire burning fire burning on the dancefloor",
+                        
+                        "I am getting so hot, i wanna take my clothes off",
+                        
+                        "It's gettin hot in here!",
+                        
+                        "I wanna make you wet",
+                        
+                        "I wanna make you sweat",
+                        
+                        "It rains, it pours, it rains, it pours",
+                        
+                        "I only wanted to see you laughing in the purple rain.",
+                        
+                        "lil' homie in my hood, when it rains it pours",
+                      
+                        "I tell her baby baby baby baby, I'm a fireball",
+
+                        "The sun is shining, and so are you",
+
+                        "Baby, it's cold outside",
+
+                        "Cloudy with a chance of meatballs",
+
+                        "Blowing with the wind, of change",
+
+                        "It's raining men, Hallelujah!",
+
+                        "You better run, you better take cover",
+
+                        "Cant you hear, can't you hear the thunder",
+
+                        "Man's not hot"];
+  
+ 
+
+    randomQuote = this.quoteList[Math.floor(Math.random() * this.quoteList.length)];
+   
+
+  displayRandomQuote() {
+    
+    window.onload= () => document.getElementById("weather-subtitle").innerHTML= this.randomQuote;
   }
 
 }
