68ad9a35d90c1adfa81d7ca56076569b4085d866
MarcusTJensen <31272825+MarcusTJensen@users.noreply.github.com>
2018-11-04 23:43:56 +0100

    profile page while not logged in

--- sightzing/src/pages/profile/profile.ts
+++ sightzing/src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the ProfilePage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewController: ViewController, public modalController: ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+  }
+
+  dismiss() {
+    this.viewController.dismiss(this.modalController);
   }
 
 }
