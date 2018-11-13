90d812cf080c46eb640f1e11d8f0e5cb6e69318d
Markus Jahnsrud <markus@soriko.no>
2018-11-13 15:28:08 +0100

    Improved experience when buying tickets

--- sightzing/src/pages/tickets-buy/tickets-buy.ts
+++ sightzing/src/pages/tickets-buy/tickets-buy.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the TicketsBuyPage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TicketsBuyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewController: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TicketsBuyPage');
+  }
+
+  dismiss() {
+    this.viewController.dismiss();
   }
 
 }
