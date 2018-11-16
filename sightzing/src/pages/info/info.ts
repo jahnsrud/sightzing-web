3b05d630dabe0f0fb0611c9fb5572480197c9bad
stinesandberg <stine.grafisk@gmail.com>
2018-11-16 16:11:39 +0100

    Fixed styling on titles on Featured slider, Fixed dynamic height of boxes on Info

--- sightzing/src/pages/info/info.ts
+++ sightzing/src/pages/info/info.ts
@@ -34,7 +34,7 @@ export class InfoPage {
         iconName: "add"},
       {
         title: 'Public Transportation',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         expanded: false,
         iconName: "add"},
       {
